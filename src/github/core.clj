4c8bc7a503325fa1ee771bb535f197d70dbc5081
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2011-11-10 19:50:09 -0600

    add function to set HTTP proxy

--- src/github/core.clj
+++ src/github/core.clj
@@ -4,6 +4,8 @@
                                                 PullRequestService
                                                 GistService)))
 
+; don't have a way to use GitHub:FI if this needs to override the GitHubClient URL
+
 (defn client
   "create a GitHubClient that can communicate with GitHub."
   ([token] (-> (GitHubClient.) (.setOauth2Token token)))
@@ -13,4 +15,10 @@
   "List repositories for a given username"
   [username]
   (-> (RepositoryService.) (.getRepositories username)))
+
+(defn http-proxy
+  [^String host ^String port]
+  (do
+    (System/setProperty "http.proxyHost" host)
+    (System/setProperty "http.proxyPort" (str port)))) ; in case they pass a Long into it
 
